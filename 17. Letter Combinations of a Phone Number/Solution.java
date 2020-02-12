f16a2a56a304379b56c836229c35b80f9f0a2394
mikezy <excellentzhuo@gmail.com>
2020-02-11 20:45:13 -0800

    Update Solution.java

--- 17. Letter Combinations of a Phone Number/Solution.java
+++ 17. Letter Combinations of a Phone Number/Solution.java
@@ -1,6 +1,40 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        String[] d = new String[]{" ", 
+//         String[] d = new String[]{" ", 
+//                                 "", 
+//                                 "abc", 
+//                                 "def",
+//                                 "ghi",
+//                                 "jkl",
+//                                 "mno",
+//                                 "pqrs",
+//                                 "tuv",
+//                                 "wxyz"};        
+//       char[] cur = new char[digits.length()];
+//       List<String> ans = new ArrayList<>();
+//       if (digits == null || digits.length() == 0) return ans; 
+//       dfs(digits, d, 0, cur, ans);
+//       return ans;
+//     }
+    
+//     private void dfs (String digits, String[] d, int l, char[] cur, List<String> ans) {
+//         //termination case
+//         if (l == digits.length()) {
+//             ans.add(new String(cur));//make a copy
+//             return;
+//         }
+//         //get the String from d[]
+//         String s = d[Character.getNumericValue(digits.charAt(l))];
+//         //loop through s
+//         for (int i = 0; i < s.length(); i++) {
+//             cur[l] = s.charAt(i);
+//             dfs(digits, d, l + 1, cur, ans);
+//         }
+        
+        //BFS, slower
+              if (digits.length() == 0) return new ArrayList<String>();
+      
+      String[] d = new String[]{" ", 
                                 "", 
                                 "abc", 
                                 "def",
@@ -9,26 +43,20 @@ class Solution {
                                 "mno",
                                 "pqrs",
                                 "tuv",
-                                "wxyz"};        
-      char[] cur = new char[digits.length()];
+                                "wxyz"};       
       List<String> ans = new ArrayList<>();
-      if (digits == null || digits.length() == 0) return ans; 
-      dfs(digits, d, 0, cur, ans);
+      ans.add("");
+      for (char digit : digits.toCharArray()) {
+        List<String> tmp = new ArrayList<>();
+        for (String t : ans) {
+          String s = d[Character.getNumericValue(digit)];
+          for (int i = 0; i < s.length(); ++i)
+            tmp.add(t + s.charAt(i));
+        }
+        ans = tmp;
+      }
+      
       return ans;
-    }
     
-    private void dfs (String digits, String[] d, int l, char[] cur, List<String> ans) {
-        //termination case
-        if (l == digits.length()) {
-            ans.add(new String(cur));//make a copy
-            return;
-        }
-        //get the String from d[]
-        String s = d[Character.getNumericValue(digits.charAt(l))];
-        //loop through s
-        for (int i = 0; i < s.length(); i++) {
-            cur[l] = s.charAt(i);
-            dfs(digits, d, l + 1, cur, ans);
-        }
     }
 }
