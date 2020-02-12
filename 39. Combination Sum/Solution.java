9b665c4d0de5894e36b91a1ba616e0e7c5cbeab9
mikezy <excellentzhuo@gmail.com>
2020-02-11 22:04:47 -0800

    add 39 40

---
+++ 39. Combination Sum/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        //corner case
+        if (candidates == null || candidates.length == 0) return res;
+        //dfs
+        dfs(candidates, target, 0, res, new ArrayList<>());
+        //return
+        return res;
+    }
+    
+    private void dfs(int[] candidates, int target, int index, List<List<Integer>> res, List<Integer> subset) {
+        //termination case 1, found the List
+        if (target == 0) {
+            res.add(new ArrayList<Integer>(subset));//add the subset to the res
+            return;
+        }
+        //termination case 2, sum of the sublist > target
+        if (target < 0) {
+            return;
+        }
+        
+        //loop through the candidates
+        for (int i = index; i < candidates.length; i++) {
+            subset.add(candidates[i]);//stack push
+            dfs(candidates, target - candidates[i], i, res, subset); //i: use all the elem in candidates, i+1: use the next elem in function
+            subset.remove(subset.size() - 1);//stack pop
+        }
+    }
+}
---
+++ 40. Combination Sum II/Solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        //corner case
+        if (candidates == null || candidates.length == 0) return res;
+        //sort candidates
+        Arrays.sort(candidates);
+        
+        dfs(candidates, target, 0, res, new ArrayList<>());
+        
+        return res;
+    }
+    
+    private void dfs(int[] candidates, int target, int index, List<List<Integer>> res, List<Integer> subset) {
+        //termination cases 
+        if (target == 0) {//found: add to res, return;
+            res.add(new ArrayList<Integer>(subset));//need to copy subset
+            return;    
+        }
+        if (target < 0) {//stop dfs, return
+            return;            
+        }
+        //loop through elems in candidates
+        for (int i = index; i < candidates.length; i++) {
+            if (i > index && candidates[i] == candidates[i - 1]) continue;//green tea
+            subset.add(candidates[i]);//stack push
+            dfs(candidates, target - candidates[i], i + 1, res, subset);//i+1: elem only use once
+            subset.remove(subset.size() - 1);//stack pop
+        }
+    }
+}
